fbc8701c26ed6336531f83bb0f91a4d888e7674e
andy0000-droid <andy020207@gmail.com>
2023-09-11 14:21:42 +0900

    error fix

--- src/kem/kyber/pqcrystals-kyber_kyber1024_avx2/kem.h
+++ src/kem/kyber/pqcrystals-kyber_kyber1024_avx2/kem.h
@@ -36,6 +36,6 @@ int crypto_kem_keypair(uint8_t *pk, uint8_t *sk);
 int crypto_kem_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk, const uint8_t *m);
 
 #define crypto_kem_dec KYBER_NAMESPACE(dec)
-int crypto_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
+int crypto_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk, uint8_t *message);
 
 #endif
