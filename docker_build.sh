71949507732718a5cefc6ae931693e5dc859dd3a
andy0000-droid <andy020207@gmail.com>
2023-09-14 19:06:45 +0900

    update shell

--- docker_build.sh
+++ docker_build.sh
@@ -6,6 +6,9 @@ echo "Build Docker image"
 rm -rf liboqs
 git clone --depth 1 --branch oqs https://github.com/andy0000-droid/OQS_Project.git liboqs
 
+#download requires
+sudo apt install astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind
+
 #build liboqs
 cd liboqs && mkdir build && cd build && cmake -GNinja .. && ninja
 echo "test message" > plaintext.txt #file exist in build folder
