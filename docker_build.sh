03e01a3aafa7e9dba29246f24a9bd9ae0c299413
andy0000-droid <andy020207@gmail.com>
2023-09-16 13:41:35 +0000

    temp update

--- docker_build.sh
+++ docker_build.sh
@@ -2,14 +2,23 @@
 
 echo "Build Docker image"
 
-# remove exist build folder & git clone
-rm -rf liboqs
-git clone --depth 1 --branch oqs https://github.com/andy0000-droid/OQS_Project.git liboqs
+# # remove exist build folder & git clone
+# rm -rf liboqs
+# git clone --depth 1 --branch oqs https://github.com/andy0000-droid/OQS_Project.git liboqs
+# 
+# #download requires
+# sudo apt install astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind
+# 
+# #build liboqs
+# cd liboqs && mkdir build && cd build && cmake -GNinja .. && ninja
+# echo "test message" > plaintext.txt #file exist in build folder
 
-#download requires
-sudo apt install astyle cmake gcc ninja-build libssl-dev python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz python3-yaml valgrind
+# Add Docker folder to bulid server image
+# cd [server image]
 
-#build liboqs
-cd liboqs && mkdir build && cd build && cmake -GNinja .. && ninja
-echo "test message" > plaintext.txt #file exist in build folder
+docker build -t server:latest .
 
+# Add Docker folder to bulid client image
+# cd [client image]
+
+docker build -t client:lates .
